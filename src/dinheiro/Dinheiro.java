687962b2a8e67a6ed20651f82a8dfcb675d03d4f
August Baumgartner <augustbaumneto@gmail.com>
2022-11-24 19:54:14 -0300

    classe Dinheiro, criado metodo para checar o resto, atualização do metodo checa centavos para verificar o sinal dele, criado atributo sinal e o metodo calculaSinal, documentação da classe, retirado os sets reais, centavos e restos. Na classe operacaoDinheiro, refactos dos metodos soma e subtração, criado os metodos mesmoSinal e SinalDiferente, documentação

--- src/dinheiro/Dinheiro.java
+++ src/dinheiro/Dinheiro.java
@@ -3,8 +3,6 @@
  */
 package dinheiro;
 
-//import java.math.*;
-
 /**
  * @author August Neto
  *
@@ -14,94 +12,114 @@ public class Dinheiro {
 	private int real;
 	private int centavos;
 	private double restos;
+	private boolean positivo;
 	
-	/*
+	/**
 	 * Construtor padrão
+	 * 
 	 */
 	
 	public Dinheiro() {
 		this.real = 0;
 		this.centavos = 0;
 		this.restos = 0.0;
+		this.positivo = true;
 	}
 	
-	/*
+	/**
 	 * Construtor com valores
+	 *
+	 * @param real  	O valor inteiro de reais
+	 * @param centavos	O valor inteiro de centavos menor que 100
+	 * @param restos	O valor double de fração dos centavos
 	 */
-	
 	public Dinheiro(int real, int centavos, double restos) {
+		
+		this.real = real;
+		
 		//Verifica se o valor é válido
 		checacentavos(centavos);
-				
-		this.real = real;
 		this.centavos = centavos;
+		
+		//Verifica se o valor é válido
+		checaresto(restos);
 		this.restos = restos;
+		
+		calculaSinal();
 	}
 	
-	/*
+	/**
 	 * Construtor com valores e resto zerado
+	 * 
+	 * @param real  	O valor inteiro de reais
+	 * @param centavos	O valor inteiro de centavos menor que 100
 	 */
 	
 	public Dinheiro(int real, int centavos) {
+		this.real = real;
+		
 		//Verifica se o valor é válido
 		checacentavos(centavos);
-		this.real = real;
 		this.centavos = centavos;
+		
 		this.restos = 0.0;
+		calculaSinal();
 	}
 	
-	/*
+	/**
 	 * Construtor com entidade dinheiro
+	 * 
+	 * @param Dinheiro	O valor do tipo Dinheiro para associar
 	 */
 	
 	public Dinheiro(Dinheiro valor) {
-		//Verifica se o valor é válido
-		checacentavos(valor.getCentavos());
 		
 		this.real = valor.getReal();
+		
+		//Verifica se o valor é válido
+		checacentavos(valor.getCentavos());
 		this.centavos = valor.getCentavos();
+		
+		//Verifica se o valor é válido
+		checaresto(valor.getRestos());
 		this.restos = valor.getRestos();
+		
+		calculaSinal();
 	}
 	
 	
-	/*
-	 * Métodos Get
+	/**
+	 * @return centavos o valor de centavos armazenados
 	 */
-	
 	public int getCentavos() {
 		return this.centavos;
 	}
 	
+	/**
+	 * @return real o valor em reais armazenados
+	 */
 	public int getReal() {
 		return this.real;
 	}
 	
+	/**
+	 * @return restos o valor do resto armazenado
+	 */
 	public double getRestos() {
 		return this.restos;
 	}
-	
-	/*
-	 * Métodos set
+
+	/**
+	 * @return positivo o sinal do dinheiro
 	 */
-	
-	public void setCentavos(int centavos) {
+	public boolean getSinal() {
+		return this.positivo;
+	}
 		
-		//Verifica se o valor é válido
-		checacentavos(centavos);
-		this.centavos = centavos;
-		
-	}
-	
-	public void setReal(int real) {
-		this.real = real;
-	}
-	
-	public void setRestos(double restos) {
-		this.restos = restos;
-	}
-	
-	/*
+	/**
 	 * Retorna o valor do dinheiro em double para cálculos
+	 * 
+	 * @return  Valor em double do tipo XX,YY, com reais e centavos (resto é desconsiderado)
 	 */
 	
 	public double retornaemDouble() {
@@ -109,8 +127,10 @@ public class Dinheiro {
 		
 	}
 	
-	/*
+	/**
 	 *  Verifica se centavos esta entre 0 e 100
+	 *  
+	 *  @param	valor	O valor para checar se é válido
 	 */
 	private void checacentavos(int valor) {
 		int modulo = Math.abs(valor);
@@ -118,10 +138,63 @@ public class Dinheiro {
 		//Se valor estiver fora quebra o código
 		if (modulo>=100) {
 			IllegalArgumentException errocentavos = new IllegalArgumentException();
-			System.out.println("Centavos acima de 99 ou menos de 0");
+			System.out.println("Centavos acima de 99");
 			throw errocentavos;
 		}	
+		
+		//Se valor estiver com sinal diferente de real 
+		if ((valor>0 && this.real < 0) || (valor<0 && this.real>0) ) {
+			IllegalArgumentException errocentavos = new IllegalArgumentException();
+			System.out.println("Centavos com sinal incorreto");
+			throw errocentavos;
+		}
+		
 	}
 	
+	/**
+	 *  Verifica se centavos esta entre 0 e 100
+	 *  
+	 *  @param	valor	O valor para checar se o resto é válido
+	 */
+	private void checaresto(double valor) {
+		double modulo = Math.abs(valor);
+		
+		//Se valor estiver fora quebra o código
+		if (modulo>=1) {
+			IllegalArgumentException erroresto = new IllegalArgumentException();
+			System.out.println("Resto acima de 1");
+			throw erroresto;
+		}	
+		
+		//Se valor estiver com sinal diferente de real 
+		if ((valor>0 && this.real < 0) || (valor<0 && this.real>0) ) {
+			IllegalArgumentException erroresto = new IllegalArgumentException();
+			System.out.println("Resto com sinal incorreto");
+			throw erroresto;
+		}
+		
+	}
+	
+	/**
+	 * 
+	 * Calcula o sinal do dinheiro se positivo dinheiro é positivo.
+	 * 
+	 */
+	
+	private void calculaSinal(){
+		// Se algum dos atibutos é maior que zero
+		if(this.real>0 || this.centavos>0 || this.restos>0) {
+			this.positivo = true;
+		} else {
+			// Se algum dos atributos é menor que zero
+			if (this.real<0|| this.centavos<0 || this.restos<0) {
+				this.positivo = false;
+			} else {
+				// Se é tudo zerado
+				this.positivo = true;
+			}
+		}
+		
+	}
 	
 }
--- src/dinheiro/OperacaoDinheiro.java
+++ src/dinheiro/OperacaoDinheiro.java
@@ -10,78 +10,179 @@ package dinheiro;
 public class OperacaoDinheiro {
 
 	//Construtor básico
+	/**
+	 * Construtor apenas para criar o objeto
+	 * 
+	 */
 	public OperacaoDinheiro() {
 		
 	}
 	
+	
+	/**
+	 * Soma dos valores de Dinheiro
+	 * 
+	 * @param fator1  	O primeiro fator
+	 * @param fator2  	O primeiro fator
+	 * @return Dinheiro	O resultado da soma
+	 */
 	public Dinheiro soma(Dinheiro fator1, Dinheiro fator2) {
+		Dinheiro resultado;
 		
+		//Verifica se tem mesmo sinal
+		if (fator1.getSinal()==fator2.getSinal()) {
+			resultado = mesmoSinal(fator1, fator2);
+			
+			
+		} else {
+			Dinheiro invfator;
+			//Verifica qual o positivo
+			if (fator1.getSinal()) {
+				//Inverte para fazer a diferença
+				invfator = new Dinheiro(fator2.getReal()*-1, fator2.getCentavos()*-1, fator2.getRestos()*-1);				
+				resultado = sinalDiferente(fator1, invfator);				
+				
+			}else {
+				//Inverte para fazer a diferença
+				invfator = new Dinheiro(fator1.getReal()*-1, fator1.getCentavos()*-1, fator1.getRestos()*-1);
+				resultado = sinalDiferente(fator2, invfator);
+			}
+			
+		}
+		
+		return resultado;
+	}
+		
+	
+	/**
+	 * Calcula a diferença do valor
+	 * 
+	 * @param fator1	O Valor atual
+	 * @param fator2	O valor a ser subtraido
+	 * @return Dinheiro	O resultado 
+	 */
+	public Dinheiro subtracao(Dinheiro fator1, Dinheiro fator2) {
+		Dinheiro resultado;
+		//Verifica se tem mesmo sinal
+		if (fator1.getSinal()==fator2.getSinal()) {
+			resultado = sinalDiferente(fator1, fator2);
+			
+			
+		} else {
+		
+			Dinheiro invfator;
+			//Iverte para fazer a diferença
+			invfator = new Dinheiro(fator2.getReal()*-1, fator2.getCentavos()*-1, fator2.getRestos()*-1);				
+			resultado = mesmoSinal(fator1, invfator);				
+		}
+			
+		
+		return resultado;
+	}	
+	
+	
+	/**
+	 * Soma dos objetos do tipo dinheiro com o mesmo sinal calculando os limites para não extrapolar centavos e nem resto
+	 * 
+	 * @param fator1		o primeiro fator da soma
+	 * @param fator2		o segundo fator da soma
+	 * @return Dinheiro		o resultado da soma
+	 */
+	private Dinheiro mesmoSinal(Dinheiro fator1, Dinheiro fator2) {
+		Dinheiro resultado;
+		
+		//Somatório dos valores
 		double somarestos= fator1.getRestos()+fator2.getRestos();
-		int somacentavos = 0;
-		int somareais=0;
+		int somacentavos = fator1.getCentavos()+fator2.getCentavos();
+		int somareais=fator1.getReal()+fator2.getReal();
 		
-		//Se maior que um, soma um nos centavos
+		//Se maior ou igual a um, soma um nos centavos
 		if (somarestos>=1) {
 			somacentavos=somacentavos+1;
 			somarestos = somarestos-1;
+		} else {
+			//Se menor ou igual a um, subtrai um nos centavos
+			if (somarestos<=-1) {
+					somacentavos=somacentavos-1;
+					somarestos = somarestos+1;
+			}
 		}
 		
-		somacentavos = somacentavos+fator1.getCentavos()+fator2.getCentavos();
-		
-		//Se maior que 100, soma um nos reais
+		//Se maior ou igual a 100, soma um nos reais
 		if (somacentavos>=100) {
 			somacentavos=somacentavos-100;
 			somareais = somareais+1;
+		} else {
+			//Se menor ou igual a 100, subtrai um nos reais
+			if (somacentavos<=-100) {
+				somacentavos=somacentavos+100;
+				somareais = somareais-1;
+			}
 		}
+		//Prepara a saída
+		resultado = new Dinheiro(somareais, somacentavos, somarestos);
 		
-		somareais = somareais+fator1.getReal()+fator2.getReal();
-		
-		Dinheiro resultado = new Dinheiro();
-		resultado.setReal(somareais);
-		resultado.setCentavos(somacentavos);
-		resultado.setRestos(somarestos);
 		return resultado;
+		
 	}
 	
 	
-public Dinheiro subtracao (Dinheiro fator1, Dinheiro fator2) {
+/**
+ * 
+ * Soma os objetos do tipo dinheiro com sinais diferentes verificando e tratando a troca de sinais
+ * 
+ * @param fator1 		O valor atual
+ * @param fator2		O valor a ser subtraido
+ * @return	Dinheiro	O resultado em Dinheiro	
+ */
+private Dinheiro sinalDiferente (Dinheiro fator1, Dinheiro fator2) {
 		
+		Dinheiro resultado;
+		
+		//Faz todas as subtrações de cada item
 		double subtrairestos= fator1.getRestos()-fator2.getRestos();
 		int subtraicentavos = fator1.getCentavos()-fator2.getCentavos();
 		int subtraireais=fator1.getReal()-fator2.getReal();
-		
+	
+		//Verifica se o resultado é negativo
 		if (subtraireais<0) {
-			
+		
+			//Verifica se o resto é positivo e transforma em negativo
 			if (subtrairestos>0) {
 				subtrairestos=subtrairestos-1.0;
 				subtraicentavos = subtraicentavos+1;
 			}
 		
-			
+			//Verifica se o centavos é positivo e transforma em negativo
 			if (subtraicentavos>0) {
 				subtraireais=subtraireais+1;
 				subtraicentavos = subtraicentavos-100;
 			}
 			
 		}else {
+			//Verifica se o resultado é positivo
 			if (subtraireais>0) {	
 				
+				//Verifica se o resto é negativo e transforma em positivo
 				if (subtrairestos<0) {
 					subtrairestos=subtrairestos+1.0;
 					subtraicentavos = subtraicentavos-1;
 				}
 			
-			
+				//Verifica se centavos é negativo e transforma em positivo
 				if (subtraicentavos<0) {
 					subtraireais=subtraireais-1;
 					subtraicentavos = subtraicentavos+100;
 				}
 			} else {
+				//Se for zerado reais, checa se o sinal de centavos e restos estão diferentes
+				//Se centavos positivo e resto negativo, acerta o resto
 				if (subtrairestos<0 && subtraicentavos>0) {
 					subtrairestos=subtrairestos+1.0;
 					subtraicentavos = subtraicentavos-1;
 				} 
 				
+				//Se centavos negativo e resto positivo, acerta o resto
 				if (subtrairestos>0 && subtraicentavos<0) {
 					subtrairestos=subtrairestos-1.0;
 					subtraicentavos = subtraicentavos+1;
@@ -90,11 +191,8 @@ public Dinheiro subtracao (Dinheiro fator1, Dinheiro fator2) {
 			}
 			
 		}
-		
-		Dinheiro resultado = new Dinheiro();
-		resultado.setReal(subtraireais);
-		resultado.setCentavos(subtraicentavos);
-		resultado.setRestos(subtrairestos);
+		//Prepara a saída
+		resultado = new Dinheiro(subtraireais, subtraicentavos, subtrairestos);		
 		return resultado;
 	}
 	
